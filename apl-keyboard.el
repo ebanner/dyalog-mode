f00b76fba14236488036e907359798f2801efa16
Edward Banner <edward@Mac.lan>
2026-01-02 14:39:50 -0500

    Simplify apl-keyboard.el

--- apl-keyboard.el
+++ apl-keyboard.el
@@ -24,61 +24,20 @@
   "APL keyboard layout template.")
 
 
-(defun apl-keyboard-mode-kill-buffer ()
-  "Close the buffer displaying the keymap."
-  (interactive)
-  (let ((buffer (get-buffer apl-keyboard--buffer-name)))
-    (when buffer
-      (delete-windows-on buffer)
-      (kill-buffer buffer))))
-
-(defvar apl-keyboard-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "q") 'apl-keyboard-mode-kill-buffer)
-    map)
-  "Keymap for keyboard mode buffers")
-
-(define-derived-mode apl-keyboard-mode fundamental-mode "APL-Keyboard"
-  "Major mode for displaying the APL keyboard layout."
-  (use-local-map apl-keyboard-mode-map)
-  (read-only-mode 1)
-  (setq truncate-lines t))
-
-(defun apl-keyboard--make-readable-keymap ()
+(defun apl-keyboard--create-buffer ()
   "Create a buffer with the keyboard layout."
-  ;; Ensure that the buffer is recreated
-  (let ((old-buffer (get-buffer apl-keyboard--buffer-name)))
-    (when old-buffer
-      (kill-buffer old-buffer)))
-  ;; Recreate the buffer
   (let ((buffer (get-buffer-create apl-keyboard--buffer-name)))
     (with-current-buffer buffer
-      (delete-region (point-min) (point-max))
-      (insert apl-keyboard-template)
-      (apl-keyboard-mode))
+      (insert apl-keyboard-template))
     buffer))
 
-(defun apl-keyboard-show (&optional arg)
-  "Toggle the display of the keyboard help.
-When ARG is nil, toggle the display.
-If ARG is positive, always show the buffer.
-If ARG is negative, close the buffer if it is open."
-  (interactive "P")
-  (let ((keyboard-help (get-buffer apl-keyboard--buffer-name)))
-    (if (and keyboard-help (get-buffer-window keyboard-help))
-        ;; The buffer is displayed. Maybe close it.
-        (when (or (null arg) (cl-minusp arg))
-          (apl-keyboard-mode-kill-buffer))
-      ;; The buffer is not displayed, check if it's supposed to be displayed
-      (when (or (null arg) (cl-plusp arg))
-        (let* ((buffer (or (when nil ; Make sure the buffer is always created
-                             (get-buffer apl-keyboard--buffer-name))
-                           (apl-keyboard--make-readable-keymap)))
-               (window (split-window nil)))
-          (set-window-buffer window buffer)
-          (fit-window-to-buffer window))))))
+(defun apl-keyboard ()
+  "Display the keyboard layout in a new window."
+  (interactive)
+  (let* ((buffer (apl-keyboard--create-buffer))
+         (window (split-window nil)))
+    (set-window-buffer window buffer)
+    (fit-window-to-buffer window)))
 
 (provide 'apl-keyboard-standalone)
 ;;; apl-keyboard-standalone.el ends here
-
-
