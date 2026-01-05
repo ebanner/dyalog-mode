ac13fc39b0360d2fdc3e8a9e256ed0db26e5112b
Edward Banner <edward@Mac.lan>
2026-01-05 16:29:55 -0500

    Comint call to create *Dyalog* buffer

--- dyalog-mode.el
+++ dyalog-mode.el
@@ -4,18 +4,3 @@
  "Dyalog"           ; process name
  "*Dyalog*"         ; buffer name
  "dyalog")          ; program to run
-
-(setq proc (get-buffer-process "*Dyalog*"))
-(setq output "")
-
-(set-process-filter
- proc
- (lambda (_proc chunk)
-   (setq output (concat output chunk))))
-
-(process-send-string proc "6\n")
-
-(accept-process-output proc 1.0) ;; let output arrive
-
-(message output)
-
