e5caa6b2d34ec0138b4fd0330b4a3414cfabd037
Edward Banner <edward@Mac.lan>
2026-01-06 14:03:04 -0500

    Use ansi-term instead of comint

--- dyalog-mode.el
+++ dyalog-mode.el
@@ -1,6 +1,11 @@
-(require 'comint)
+; (require 'comint)
 
-(make-comint-in-buffer
- "Dyalog"           ; process name
- "*Dyalog*"         ; buffer name
- "dyalog")          ; program to run
+; (make-comint-in-buffer
+;  "Dyalog"           ; process name
+;  "*Dyalog*"         ; buffer name
+;  "dyalog")          ; program to run
+
+(with-eval-after-load 'term
+  ;; Paste into the terminal (char-mode)
+  (define-key term-raw-map (kbd "C-y") #'term-paste))
+
