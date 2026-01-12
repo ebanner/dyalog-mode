853d24dffe468c0107195e6aa5d70f98238bda3b
Edward Banner <edward@Mac.lan>
2026-01-12 14:30:35 -0500

    Use comint

--- dyalog-mode.el
+++ dyalog-mode.el
@@ -1,11 +1,12 @@
-; (require 'comint)
+(defun dyalog--input-sender (proc input)
+  ;; Send INPUT plus carriage return.
+  (comint-send-string proc input)
+  (comint-send-string proc "\r"))
 
-; (make-comint-in-buffer
-;  "Dyalog"           ; process name
-;  "*Dyalog*"         ; buffer name
-;  "dyalog")          ; program to run
-
-(with-eval-after-load 'term
-  ;; Paste into the terminal (char-mode)
-  (define-key term-raw-map (kbd "C-y") #'term-paste))
-
+(defun dyalog-comint ()
+  (interactive)
+  (let ((process-connection-type t))
+    (make-comint-in-buffer "Dyalog" "*Dyalog*" "dyalog" nil "-s"))
+  (pop-to-buffer "*Dyalog*")
+  (with-current-buffer "*Dyalog*"
+    (setq-local comint-input-sender #'dyalog--input-sender)))
